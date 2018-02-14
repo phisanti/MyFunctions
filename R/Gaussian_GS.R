d9dd6f7b47e2a132683156fe38639cfe857a7862
phisanti <tisalon@outlook.com>
2018-02-14 16:54:55 +0000

    Correct burn.in

--- R/Gaussian_GS.R
+++ R/Gaussian_GS.R
@@ -4,16 +4,18 @@
 #' @param niter Number of iterations to extract samples
 #' @param prior.mu Starting value of mu
 #' @param prior.sd Starting value of sigma
+#' @param burn.in Warm up samples
 #'
 #' @author Santiago Caño-Muñiz
 #' @export
 
-Gaussian_GS <- function(x, niter = 2000, prior.mu = 0, prior.sd =1) {
+
+Gaussian_GS <- function(x, niter = 2000, prior.mu = 0, prior.sd =1, burn.in = 200) {
   # Default priors
   if (prior.mu == 0) {prio.mu <- mean(x)} # Fill prior mu with empirical mu
-  if (prior.sd == 1) {prio.sd <- sd(x)} # Fill prior sd with empirical sd
+  if (prior.sd == 1) {prior.sd <- sd(x)} # Fill prior sd with empirical sd
   mu.cur <- prior.mu
-  psi.cur <- 1/prio.sd^2 # Calculate psi
+  psi.cur <- 1/prior.sd^2 # Calculate psi
   # Output matrix
   sim.gibbs <- matrix(0, nrow = niter + 1, ncol = 2)
   colnames(sim.gibbs) <- c('mu', 'sigma')
@@ -32,6 +34,7 @@ Gaussian_GS <- function(x, niter = 2000, prior.mu = 0, prior.sd =1) {
     sim.gibbs[i + 1, 1] <- mu.cur
     sim.gibbs[i + 1, 2] <- psi.cur
   }
+  sim.gibbs <- sim.gibbs[-c(1:burn.in),] # Remove burn.in
 
   # Transform psi to sigma
   sim.gibbs[,2] <- 1/sqrt(sim.gibbs[,2])
