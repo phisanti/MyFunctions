974069b94b048fe3734ca2321ae6f907ace82a44
S. CM <33370667+phisanti@users.noreply.github.com>
2021-09-12 12:52:34 +0100

    Add base feature on the scientific notation

--- R/scientific_axis.R
+++ R/scientific_axis.R
@@ -1,17 +1,19 @@
 #' @title Scientific axis
 #' @description Transform a numeric vector into a character expression in scietific format
-#' @param x numeric vector to be transform
+#' @param x numeric: vector to be transform
+#' @param base integer: Base for the exponent
 #' @author Santiago Caño-Muñiz
 #' @export
 
 
 # Scientific axis
 #this function writes the axis numbers in scientific formant
-scientific_axis <- function(x) {
+scientific_axis <- function(x, base = 10) {
   # turn in to character string in scientific notation
   x <- paste0(1,"e",x)
   # turn the 'e+' into plotmath format
-  x <- gsub("e", "%*%10^", x)
+  base <- paste0("%*%", as.character(base), "^")
+  x <- gsub("e", base, x)
   # return this as an expression
   parse(text = x)
 }
